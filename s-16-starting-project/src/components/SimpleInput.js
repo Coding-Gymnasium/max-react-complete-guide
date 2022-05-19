19cbfb6d41b1b5804d5c28702e52782cd377f83d
Nico Rithner <nicorithner@gmail.com>
2022-05-19 17:15:51 -0600

    Complete lesson 200

--- s-16-starting-project/src/components/SimpleInput.js
+++ s-16-starting-project/src/components/SimpleInput.js
@@ -1,9 +1,34 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputedChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue)
+
+    // nameInputRef.current.value = ''; => NOT BEST PRACTICE, Don't manipute the DOM
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputedChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
