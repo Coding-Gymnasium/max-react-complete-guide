115efb79985ae7100563bf7a8b1452ce81c66b74
Nico Rithner <nicorithner@gmail.com>
2025-02-26 11:16:44 -0700

    Use dynamic value syntax to import image

--- 2025-course/01-starting-project/src/App.jsx
+++ 2025-course/01-starting-project/src/App.jsx
@@ -1,3 +1,4 @@
+import reactImage from "./assets/react-core-concepts.png";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 const max = reactDescriptions.length - 1;
 
@@ -9,7 +10,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(max)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
