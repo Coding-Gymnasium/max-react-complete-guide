d6727d4669c83350a87e5f1439e2e834ea0bcdb2
Nico Rithner <nicorithner@gmail.com>
2025-02-26 11:11:25 -0700

    Add outputting dynamic values using curly braces in jsx

--- 2025-course/01-starting-project/src/App.jsx
+++ 2025-course/01-starting-project/src/App.jsx
@@ -1,10 +1,18 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+const max = reactDescriptions.length - 1;
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(max)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
