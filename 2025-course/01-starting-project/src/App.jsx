173d6650d1302a5c262684643e2fdd1f158b5195
Nico Rithner <nicorithner@gmail.com>
2025-02-26 19:11:48 -0700

    Migrate each component to its own file

--- 2025-course/01-starting-project/src/App.jsx
+++ 2025-course/01-starting-project/src/App.jsx
@@ -1,36 +1,6 @@
-import reactImage from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-const max = reactDescriptions.length - 1;
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(max)];
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header";
+import { CoreConcept } from "./components/CoreConcept";
 
 function App() {
   return (
@@ -42,12 +12,7 @@ function App() {
           <ul>
             {CORE_CONCEPTS.map((item) => (
               <li key={item.id}>
-                <CoreConcept
-                  {...item}
-                  // title={item.title}
-                  // description={item.description}
-                  // image={item.image}
-                />
+                <CoreConcept {...item} />
               </li>
             ))}
           </ul>
---
+++ 2025-course/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import reactImage from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+const max = reactDescriptions.length - 1;
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescriptions[genRandomInt(max)];
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
