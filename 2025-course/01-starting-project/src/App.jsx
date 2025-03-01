fc957454ef156f6cfa9d20867674dcb9dd8929f2
Nico Rithner <nicorithner@gmail.com>
2025-03-01 13:29:39 -0700

    Add useState to handle tab content

--- 2025-course/01-starting-project/src/App.jsx
+++ 2025-course/01-starting-project/src/App.jsx
@@ -1,8 +1,16 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/header/Header";
 import { CoreConcept } from "./components/coreConcepts/CoreConcept";
+import TabButton from "./components/tabButton/TabButton";
+import { useState } from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Select a button");
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
   return (
     <div>
       <Header />
@@ -17,6 +25,18 @@ function App() {
             ))}
           </ul>
         </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+        </section>
+        {selectedTopic}
       </main>
     </div>
   );
