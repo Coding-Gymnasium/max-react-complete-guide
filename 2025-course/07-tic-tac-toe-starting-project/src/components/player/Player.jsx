91b794ffe157ee10c39dcf3e5a8aa24d7533fcd8
Nico Rithner <nicorithner@gmail.com>
2025-03-09 11:21:15 -0600

    Add editable player name

--- 2025-course/07-tic-tac-toe-starting-project/src/components/player/Player.jsx
+++ 2025-course/07-tic-tac-toe-starting-project/src/components/player/Player.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputName, setInputName] = useState("");
+  const [editButtonActive, setEditButtonActive] = useState(false);
+
+  function handleSubmit() {
+    setIsEditing(!isEditing);
+    setEditButtonActive(!editButtonActive);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? (
+          <input
+            type="text"
+            value={inputName}
+            onChange={(e) => setInputName(e.target.value)}
+          />
+        ) : (
+          <span className="player-name">{inputName ? inputName : name}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleSubmit}>
+        {editButtonActive ? "Save" : "Edit"}
+      </button>
     </li>
   );
 }
