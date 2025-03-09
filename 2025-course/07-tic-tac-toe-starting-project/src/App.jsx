160704023e86e53d160d5c7b5e1c55a2b0f5420e
Nico Rithner <nicorithner@gmail.com>
2025-03-09 09:09:39 -0600

    Create player component

--- 2025-course/07-tic-tac-toe-starting-project/src/App.jsx
+++ 2025-course/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./components/player/Player";
+
 function App() {
-  return <p style={{ color: "black" }}>Coming soon....</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
