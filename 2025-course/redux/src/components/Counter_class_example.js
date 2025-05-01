7dfeca5d31aa421325d759209c2c9c815078a210
Nico Rithner <nicorithner@gmail.com>
2025-05-01 16:40:42 -0600

    Add example with class component

---
+++ 2025-course/redux/src/components/Counter_class_example.js
@@ -0,0 +1,47 @@
+import { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import classes from "./Counter.module.css";
+
+class Counter extends Component {
+  incrementHandler() {
+    this.props.increment();
+  }
+  decrementHandler() {
+    this.props.decrement();
+  }
+  toggleCounterHandler() {}
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+Counter.propTypes = {
+  counter: PropTypes.number.isRequired,
+  increment: PropTypes.func.isRequired,
+  decrement: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "increment" }),
+    decrement: () => dispatch({ type: "decrement" }),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ 2025-course/redux/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "redux";
+
+// reducer
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return { ...state, counter: state.counter + 1 };
+  }
+  if (action.type === "decrement") {
+    return { ...state, counter: state.counter - 1 };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+export default store;
