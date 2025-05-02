be3069cc6692439817bde0f7ea5d0f251fd394e4
Nico Rithner <nicorithner@gmail.com>
2025-05-02 10:33:31 -0600

    Add toggle using redux

--- 2025-course/redux/src/store/index.js
+++ 2025-course/redux/src/store/index.js
@@ -1,16 +1,36 @@
 import { createStore } from "redux";
-
+const initialState = { counter: 0, showCounter: true };
 // reducer
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
-    return { ...state, counter: state.counter + 1 };
+    return {
+      ...state,
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
   }
   if (action.type === "decrement") {
-    return { ...state, counter: state.counter - 1 };
+    return {
+      ...state,
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
   }
 
   if (action.type === "increase") {
-    return { ...state, counter: state.counter + action.amount };
+    return {
+      ...state,
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
   }
 
   return state;
