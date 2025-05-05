7d91f345530d8b95815fa83959517a7988d378bf
Nico Rithner <nicorithner@gmail.com>
2025-05-05 16:19:00 -0600

    Set up login logout function

--- 2025-course/redux/src/App.js
+++ 2025-course/redux/src/App.js
@@ -1,12 +1,17 @@
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuthenticated = useSelector(
+    (state) => state.authReducer.isAuthenticated
+  );
   return (
     <>
       <Header />
-      <Auth />
+      {isAuthenticated ? <UserProfile /> : <Auth />}
       <Counter />
     </>
   );
