4ac6e86558943a0ab69e59f32e8fca578ecd699e
Nico Rithner <nicorithner@gmail.com>
2025-05-07 15:35:10 -0600

    Add cart toggle

--- 2025-course/advanced-redux/src/components/Cart/CartButton.js
+++ 2025-course/advanced-redux/src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const handleCartToggle = (event) => {
+    event.preventDefault();
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button onClick={handleCartToggle} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
