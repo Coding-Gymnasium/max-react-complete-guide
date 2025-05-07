fff244702e281fe33c78e541978f72a14b8095a0
Nico Rithner <nicorithner@gmail.com>
2025-05-07 15:40:39 -0600

    Refactor using Max's approach

--- 2025-course/advanced-redux/src/components/Cart/Cart.js
+++ 2025-course/advanced-redux/src/components/Cart/Cart.js
@@ -1,23 +1,17 @@
-import { useSelector } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
 
 const Cart = (props) => {
-  const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
   return (
-    <>
-      {cartIsVisible && (
-        <Card className={classes.cart}>
-          <h2>Your Shopping Cart</h2>
-          <ul>
-            <CartItem
-              item={{ title: "Test Item", quantity: 3, total: 18, price: 6 }}
-            />
-          </ul>
-        </Card>
-      )}
-    </>
+    <Card className={classes.cart}>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        <CartItem
+          item={{ title: "Test Item", quantity: 3, total: 18, price: 6 }}
+        />
+      </ul>
+    </Card>
   );
 };
 
