14ab7d4c16856a36c5a161ba9b048902da5aca70
Nico Rithner <nicorithner@gmail.com>
2025-05-22 19:50:24 -0600

    Add add and remove items functionality

--- 2025-course/advanced-redux/src/store/cart-slice.js
+++ 2025-course/advanced-redux/src/store/cart-slice.js
@@ -10,26 +10,28 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => newItem.id === item.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
           title: newItem.title,
         });
       } else {
-        existingItem.quantity = existingItem.quantity++;
+        existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => id === item.id);
+      state.totalQuantity--;
       if (existingItem && existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
-        existingItem.quantity = existingItem.quantity--;
+        existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
